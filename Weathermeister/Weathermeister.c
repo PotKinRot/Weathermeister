3b062ca2a95bbe82b13fc5a70eaa3d8dda36c973
PotKinRot <potkinrot@web.de>
2015-06-09 14:42:39 +0200

    Creation
    Initial creation of repo with bare project data. Contains I2C and LCD
    code.

---
+++ Weathermeister/Weathermeister.c
@@ -0,0 +1,17 @@
+/*
+ * Weathermeister.c
+ *
+ * Created: 09.06.2015 14:37:49
+ *  Author: Tilmann
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
