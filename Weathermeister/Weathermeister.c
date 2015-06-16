a44d84666e9ee51db8034943cde5e7e758245446
schewi <schewidaking@freenet.de>
2015-06-16 11:40:56 +0200

    Added some Stuff, new I2C Librarys, changed someting. PENIS

--- Weathermeister/Weathermeister.c
+++ Weathermeister/Weathermeister.c
@@ -7,11 +7,22 @@
 
 
 #include <avr/io.h>
+#include <LCD.h>
+#include <I2C_master.h>
+#include <i2cfncs.h>
+#include <DS1307new.h>
+
+
 
 int main(void)
 {
+	
+	Initialize_LCD();
+	
     while(1)
     {
+		Send_Character("Test");
+		
         //TODO:: Please write your application code 
     }
 }
