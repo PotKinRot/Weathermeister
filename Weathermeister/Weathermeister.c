60898194fac3101d0cce0036d17234363e47bb7c
schewi <schewidaking@freenet.de>
2015-06-16 13:42:50 +0200

    Changed a lot of stuff, LED as output, test for Pressure sensor

--- Weathermeister/Weathermeister.c
+++ Weathermeister/Weathermeister.c
@@ -6,12 +6,49 @@
  */ 
 
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <math.h>
 
-int main(void)
-{
-    while(1)
-    {
-        //TODO:: Please write your application code 
-    }
+#include "bmp085/bmp085.h"
+
+int main(void) {
+	long l;
+	double d;
+	char printbuff[10];
+	DDRA=0xff;
+	PORTA=0x00;
+
+	//init interrupt
+	sei();
+
+	//init bmp085
+	bmp085_init();
+
+	//main loop
+	for (;;)
+	{
+		//get temperature
+		d = bmp085_gettemperature();
+		dtostrf(d, 10, 2, printbuff);
+		PORTA= l;
+
+		//get pressure
+		l = bmp085_getpressure();
+		ltoa(l, printbuff, 10);
+		
+
+		//get altitude
+		d = bmp085_getaltitude();
+		dtostrf(d, 10, 2, printbuff);
+		
+
+		_delay_ms(3000);
+	}
+	
+	return 0;
 }
