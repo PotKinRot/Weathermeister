1e1d7649a34a2ee22711c3f7ebd1e9c9d9f378ca
schewi <schewidaking@freenet.de>
2015-06-20 22:56:56 +0200

    Setup complete working. Time and date work.

--- Weathermeister/StateFncs.c
+++ Weathermeister/StateFncs.c
@@ -17,13 +17,13 @@
 #include "I2Cfncs.h"
 
 uint8_t time[6];
-uint8_t timeset[6];
 volatile int bu;
 volatile int bd;
 volatile int bs;
 volatile int bf;
 int cnt = 0;
 volatile weather1 weather;
+volatile int issetup =0;
 
 extern volatile state my_state;
 
@@ -82,10 +82,7 @@ state do_DISP_TEMP()
 		if(my_state != DISP_TEMP)	//if any interrupt has changed the destination state, shut up.
 			return my_state;
 			
-		if (bu==0 && bd == 0)
-		{
-			return DISP_TEMP;	
-		}
+
 		if (bd==1)
 		{
 			clear_display();
@@ -98,7 +95,31 @@ state do_DISP_TEMP()
 			bu=0;
 			return DISP_LIGHT;
 		}
+		
+		if (bs==1)
+		{
+			clear_display();
+			bs=0;
+			issetup=1;
+			return SET_HOUR;
+		}		
+		
+		if (bf==1)
+		{
+			clear_display();
+			bf=0;
+			return DISP_FC;
 		}
+		
+		if (bu==0 && bd == 0)
+		{
+			return DISP_TEMP;
+		}
+	
+		
+		
+		}
+		
 	
 state do_DISP_PRESS()
 {
@@ -140,10 +161,7 @@ state do_DISP_PRESS()
 		if(my_state != DISP_PRESS)	//if any interrupt has changed the destination state, shut up.
 			return my_state;
 		
-		if (bu==0 && bd == 0)
-		{
-			return DISP_PRESS;
-		}
+
 		if (bu==1)
 		{
 			clear_display();
@@ -156,6 +174,23 @@ state do_DISP_PRESS()
 			bd=0;
 			return DISP_LIGHT;
 		}		
+		if (bs==1)
+		{
+			clear_display();
+			issetup=1;
+			bs=0;
+			return SET_HOUR;
+		}
+		if (bf==1)
+		{
+			clear_display();
+			bf=0;
+			return DISP_FC;
+		}		
+		if (bu==0 && bd == 0)
+		{
+			return DISP_PRESS;
+		}
 
 }
 	
@@ -214,6 +249,25 @@ state do_DISP_HUM()
 			bd=0;
 			return DISP_PRESS;
 		}		
+		if (bs==1)
+		{
+			clear_display();
+			issetup=1;
+			bs=0;
+			return SET_HOUR;
+		}
+		if (bf==1)
+		{
+			clear_display();
+			bf=0;
+			return DISP_FC;
+		}		
+		
+		if (bu==0 && bd == 0)
+		{
+			return DISP_HUM;
+		}		
+		
 }
 	
 state do_DISP_LIGHT()
@@ -254,10 +308,7 @@ state do_DISP_LIGHT()
 		if(my_state != DISP_LIGHT)	//if any interrupt has changed the destination state, shut up.
 			return my_state;		
 		
-		if (bu==0 && bd == 0)
-		{
-			return DISP_LIGHT;
-		}
+
 		if (bu==1)
 		{
 			clear_display();
@@ -270,6 +321,29 @@ state do_DISP_LIGHT()
 			clear_display();
 			return DISP_TEMP;
 		}	
+		if (bs==1)
+		{
+			clear_display();
+			bs=0;
+			return SET_HOUR;
+		}
+		if (bf==1)
+		{
+			clear_display();
+			issetup=1;
+			bf=0;
+			return DISP_FC;
+		}		
+		
+		if (bs==0 && bf == 0)
+		{
+			return DISP_HUM;
+		}
+		
+		if (bu==0 && bd == 0)
+		{
+			return DISP_LIGHT;
+		}		
 }
 	
 state do_SLEEP()
@@ -294,33 +368,224 @@ state do_DISP_FC()
 	
 state do_SET_HOUR()
 {
-//	timeset = time;
+		GotoLCD_Location(1,1);
+		Send_String("Set Hour:");
+		GotoLCD_Location(1,2);
+		Send_Int(time[3]);
+		
+		
+		if (bu==1)
+		{
+			clear_display();
+			bu=0;
+			if (time[3]<=22)
+			time[3]++;
+			else time[3]=0;
+		}
+		if (bd==1)
+		{
+			bd=0;
+			clear_display();
+			if (time[3]>=1)
+			time[3]--;
+			else time[3]=23;
+		}
+		
+
+		if (bs==1)
+		{
+			clear_display();
+			bs=0;
+			issetup=0;
+			return DISP_TEMP;
+		}
+		if (bf==1)
+		{
+			clear_display();
+			bf=0;
+			return SET_MIN;
+		}
+		
 	return SET_HOUR;
 }
 	
 state do_SET_MIN()
 {
+	GotoLCD_Location(1,1);
+	Send_String("Set Minute:");
+	GotoLCD_Location(1,2);
+	Send_Int(time[4]);
+	
+	
+		if (bu==1)
+		{
+			clear_display();
+			bu=0;
+			if (time[4]<=58)
+			time[4]++;
+			else time[4]=0;
+		}
+		if (bd==1)
+		{
+			bd=0;
+			clear_display();
+			if (time[4]>=1)
+			time[4]--;
+			else time[4]=59;
+		}
+		
+
+	if (bs==1)
+	{
+		clear_display();
+		bs=0;
+		issetup=0;
+		return DISP_TEMP;
+	}
+	if (bf==1)
+	{
+		clear_display();
+		bf=0;
+		return SET_DAY;
+	}
+		
 	return SET_MIN;
 }
 	
 state do_SET_DAY()
 {
+	GotoLCD_Location(1,1);
+	Send_String("Set Day:");
+	GotoLCD_Location(1,2);
+	Send_Int(time[2]);
+	
+	
+	if (bu==1)
+	{
+		clear_display();
+		bu=0;
+		if (time[2]<=30)
+		time[2]++;
+		else time[2]=1;
+	}
+	if (bd==1)
+	{
+		bd=0;
+		clear_display();
+		if (time[2]>=2)
+		time[2]--;
+		else time[2]=31;
+	}
+	
+
+	if (bs==1)
+	{
+		clear_display();
+		bs=0;
+		issetup=0;
+		return DISP_TEMP;
+	}
+	if (bf==1)
+	{
+		clear_display();
+		bf=0;
+		return SET_MONTH;
+	}
+		
 	return SET_DAY;
 }
 	
 state do_SET_MONTH()
 {
+	GotoLCD_Location(1,1);
+	Send_String("Set Month:");
+	GotoLCD_Location(1,2);
+	Send_Int(time[1]);
+	
+	
+	if (bu==1)
+	{
+		clear_display();
+		bu=0;
+		if (time[1]<=11)
+		time[1]++;
+		else time[1]=1;
+	}
+	if (bd==1)
+	{
+		bd=0;
+		clear_display();
+		if (time[1]>=2)
+		time[1]--;
+		else time[1]=12;
+	}
+	
+
+	if (bs==1)
+	{
+		clear_display();
+		bs=0;
+		issetup=0;
+		return DISP_TEMP;
+	}
+	if (bf==1)
+	{
+		clear_display();
+		bf=0;
+		return SET_YEAR;
+	}	
 	return SET_MONTH;
 }
 	
 state do_SET_YEAR()
 {
+	GotoLCD_Location(1,1);
+	Send_String("Set Year:");
+	GotoLCD_Location(1,2);
+	Send_Int(time[0]);
+	
+	
+	if (bu==1)
+	{
+		clear_display();
+		bu=0;
+		if (time[0]<=99)
+		time[0]++;
+		else time[0]=14;
+	}
+	if (bd==1)
+	{
+		bd=0;
+		clear_display();
+		if (time[0]>=15)
+		time[0]--;
+		else time[0]=99;
+	}
+	
+
+	if (bs==1)
+	{
+		clear_display();
+		bs=0;
+		issetup=0;
+		return DISP_TEMP;
+	}
+	if (bf==1)
+	{
+		clear_display();
+		bf=0;
+		return DISP_SETUP_MSG;
+	}	
 	return SET_YEAR;
 }
 	
 state do_DISP_SETUP_MSG()
 {
-	return DISP_SETUP_MSG;
+	
+	ds1307_setdate(time[0],time[1],time[2],time[3],time[4],0);
+	_delay_ms(3000);
+	issetup=0;
+	return DISP_TEMP;
 }
 
 
@@ -344,7 +609,9 @@ void Debounce()
 
 void Get_Weather_Data()
 {
+	if (issetup==0)
 	ds1307_getdate(&time[0], &time[1], &time[2], &time[3], &time[4], &time[5]);
+	
 	weather.temp = bmp085_gettemperature();
 	weather.pres = bmp085_getpressure();
 	weather.hum = Get_Hum();
