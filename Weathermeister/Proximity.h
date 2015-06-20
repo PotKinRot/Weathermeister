dc22a21f0dc66829dab9aab8d26b89f52ea5d476
PotKinRot <potkinrot@web.de>
2015-06-20 14:23:02 +0200

    Created Proximity files and added code

---
+++ Weathermeister/Proximity.h
@@ -0,0 +1,16 @@
+/*
+ * Proximity.h
+ *
+ * Created: 20.06.2015 12:09:16
+ *  Author: Tilmann
+ */ 
+
+
+#ifndef PROXIMITY_H_
+#define PROXIMITY_H_
+
+void ProxySetup();
+int ProxyDetect();
+
+
+#endif /* PROXIMITY_H_ */
